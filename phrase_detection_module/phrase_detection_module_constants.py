91b7a85b86660fc926631effa6a794b1e3e9bfbe
Sameet Patil <sameetpatil5@gmail.com>
2024-12-31 11:24:05 +0530

    Clean main branch

--- phrase_detection_module/phrase_detection_module_constants.py
+++
@@ -1,6 +0,0 @@
-PHRASE_DATASET = "./datasets/phrase_datasets/phrase_dataset.csv"
-PHRASE_TEST_DATASET = "./datasets/phrase_datasets/phrase_test_dataset.csv"
-AUGMENTED_PHRASE_DATASET = "./datasets/phrase_datasets/augmented_phrase_dataset.csv"
-MEDICAL_SYNONYMS = "./datasets/phrase_datasets/medical_synonym.json"
-PHRASE_DETECTION_MODEL = "./models/phrase_detection_model.pkl"
-AUGMENTATION_VARIANCE = 2
