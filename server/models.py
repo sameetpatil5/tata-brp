24cd66e67f03145cc2cb2cc8933428092bc1f0bc
Sameet Patil <sameetpatil5@gmail.com>
2025-02-18 13:15:22 +0530

    Add validator for FilePath Model, Refactor OCRInput Model ->  Markdown Model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,24 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, HttpUrl, field_validator
+import os
 # Define a Pydantic model for the input data
-class OCRInput(BaseModel):
+class Markdown(BaseModel):
     markdown: str
+
+class FilePath(BaseModel):
+    file_path: str
+    @field_validator("file_path")
+    def validate_file_path(cls, value):
+        """
+        Validates the file path.
+        - If it's a local path, checks if the file exists.
+        - If it's a URL, checks if it's a valid HTTP(S) URL.
+        """
+        if value.startswith(("http://", "https://")):
+            # Validate URL using Pydantic
+            return HttpUrl(value)
+        
+        # Validate local file path
+        if not os.path.exists(value):
+            raise ValueError(f"File does not exist: {value}")
+
+        return value
